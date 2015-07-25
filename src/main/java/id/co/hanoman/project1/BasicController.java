b00f3c3cafd19415cd6124d99f886b24f8df8827
Agus K Pranantoseno <agus@senomas.com>
2015-07-25 18:58:48 +0700

    init

---
+++ src/main/java/id/co/hanoman/project1/BasicController.java
@@ -0,0 +1,14 @@
+package id.co.hanoman.project1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BasicController {
+
+	@RequestMapping("/")
+	public String homePage(Model model) {
+		return "index";
+	}
+}
